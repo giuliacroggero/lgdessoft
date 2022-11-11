ce16624a4d4dfde7a0100341686008388d123485
Giulia <giuliar1@al.insper.edu.br>
2022-11-11 19:52:25 -0300

    só começando

---
+++ EP2jogo.py
@@ -0,0 +1,9 @@
+from funcoes import *
+
+
+print('\33[35mOlá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n\n' )
+nome = input('\33[0mQual o seu nome? ')
+print('\33[37mOk {0}, você tem direito a pular 3 vezes e 2 ajudas!\n'.format(nome))
+print('\33[36mAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n\n\33[0mAperte Enter para continuar...\n\n')
+print('\33[0mO jogo já vai começar! Lá vem a primeira questão!\n\nVamos começar com questões do nível FÁCIL!\nAperte ENTER para continuar')
+
