28bd9fcf75f044c10349b93253cd414250ee4f03
Luizatac <luizatac@al.insper.edu.br>
2022-11-11 12:25:58 -0300

    setima funcao

---
+++ geraajudaemumaquestao.py
@@ -0,0 +1,18 @@
+import random
+def gera_ajuda(dicquestao):
+    correta = dicquestao['correta']
+    erradas = 'ABCD'.replace(correta, '')
+    qntd = random.choice([1,2])
+    if qntd == 1:
+        sorteio = random.choice(erradas)
+        dicop = dicquestao['opcoes']
+        sorteado = dicop[sorteio]
+        return 'DICA:\nOpções certamente erradas: ' + sorteado
+    else:
+        sorteio = random.choice(erradas)
+        dicop = dicquestao['opcoes']
+        sorteado = dicop[sorteio]
+        erradas = erradas.replace(sorteio, '')
+        sorteio = random.choice(erradas)
+        sorteado2 = dicop[sorteio]
+        return 'DICA:\nOpções certamente erradas: ' + sorteado + ' | '  
