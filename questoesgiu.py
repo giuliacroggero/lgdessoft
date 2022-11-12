0f7632a5aede5ba4212ea10bd073c536b01580e1
Giulia <giuliar1@al.insper.edu.br>
2022-11-12 17:23:00 -0300

    fiz 3 questoes faceis

---
+++ questoesgiu.py
@@ -0,0 +1,16 @@
+{'titulo': 'Qual o nome do presidente do Brasil eleito para 2023?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'Jair Bolsonaro', 'B': 'Luís Inácio da Silva', 'C': 'Jânio Quadros', 'D': 'Ciro Gomes'},
+          'correta': 'B'},
+
+{'titulo': 'Oque a palavra "water" significa em português?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'água', 'B': 'suco', 'C': 'doce', 'D': 'baleia'},
+          'correta': 'A'},
+
+{'titulo': 'Qual cor ao ser misturada com azul dica verde?',
+          'nivel': 'facil',
+          'opcoes': {'A': 'vermelho', 'B': 'rosa', 'C': 'preto', 'D': 'amarelo'},
+          'correta': 'D'},
+
+
