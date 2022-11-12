ab0dfe6aab4fd2ba95b9f3407a1a79008b3133ba
Giulia <giuliar1@al.insper.edu.br>
2022-11-12 16:57:18 -0300

    atualizando

--- jogo.py
+++ jogo.py
@@ -1,9 +1,10 @@
 from funcoes import *
 
-
 print('\33[35mOlá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n\n' )
 nome = input('\33[0mQual o seu nome? ')
 print('\33[37mOk {0}, você tem direito a pular 3 vezes e 2 ajudas!\n'.format(nome))
 print('\33[36mAs opções de resposta são "A", "B", "C", "D", "ajuda", "pula" e "parar"!\n\n')
 enter = input('\33[0mAperte Enter para continuar...')
-print('\33[0mO jogo já vai começar! Lá vem a primeira questão!\n\nVamos começar com questões do nível FÁCIL!\nAperte ENTER para continuar')
+print('\33[0mO jogo já vai começar! Lá vem a primeira questão!\n\nVamos começar com questões do nível FÁCIL!\n')
+enter = input('\33[0mAperte ENTER para continuar')
+print('\n\n-------------------------------------------')
